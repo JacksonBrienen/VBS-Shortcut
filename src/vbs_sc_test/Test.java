35d1e51d212ac2098049d65dad078df11a6685a6
JacksonBrienen <88233698+JacksonBrienen@users.noreply.github.com>
2021-08-28 02:48:40 -0700

    Added Try Catch to Test to keep up With Update 1.1

--- src/vbs_sc_test/Test.java
+++ src/vbs_sc_test/Test.java
@@ -1,11 +1,17 @@
 package vbs_sc_test;
 
+import java.io.FileNotFoundException;
+
 import vbs_sc.ShortcutFactory;
 
 public class Test {
 
 	public static void main(String[] args) {
-		ShortcutFactory.createDesktopShortcut("C:\\Program Files\\Internet Explorer\\iexplore.exe", "Test.lnk");
+		try {
+			ShortcutFactory.createDesktopShortcut("C:/Program Files/Internet Explorer/iexplore.exe", "Internet_Explorer.lnk");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 	
 }
